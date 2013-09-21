0754482bf4c27a2de8ba00c3a20140a7961886f7
Brian Yan <sorakeybade@gmail.com>
2013-09-21 03:14:52 -0400

    Got my functions to only take paragraph tags

--- test/htmlScraping.js
+++ test/htmlScraping.js
@@ -1,13 +1,22 @@
 var returnAllText = function(){
-    var allText = String(document.body.textContent);
-    return removeSpaces(allText);
+    var allText = document.getElementsByTagName('p');
+
+    return removeSpaces(listToString(allText));
+}
+
+var listToString = function(array){
+    var s = "";
+    for (x = 0; x<array.length;x++){
+	s = s + array[x].innerText;
+    }
+    return s;
 }
 
 var removeSpaces = function(text){
     var x = text;
-    x = x.replace(/(\r\n|\n|\r)/gm,"");
+    x = x.replace(/(\r\n|\n|\r)/gm," ");
     x = x.replace(/\t+/g," ");
+    x = x.replace(/[^a-zA-Z0-9]/g,"");
     x.trim();
-    x = x.replace('  ', ' ');
     return x;
 }
