95f1e091a418813b358e8ff8c02a8fab7f3eee0f
Brian Yan <sorakeybade@gmail.com>
2013-09-21 07:14:36 -0400

    Almost done with storage, have a few kinks to work out tho. I do not mean kinky when I say kinks... pervs

--- browser-extension/storage.js
+++ browser-extension/storage.js
@@ -2,34 +2,37 @@ var save = function(concepts){
     for(x = 0;x<concepts.length;x++)
     {
 	saveSingleConcept(concepts[x]);
+	console.log(concepts[x]);
     }
 }
 
 
 var loadText = function(){
-    return chrome.storage.local.get('text');
+    return chrome.storage.local.get('text',function(){});
 }
 
 var loadRelevance = function(){
-    return chrome.storage.local.get('relevance');
+    return chrome.storage.local.get('relevance',function(){});
 }
 
 
 
 
 var saveSingleConcept = function(concept){
-    currentText = loadText();
-    currentRelevance =loadRelevance;
-    if (currentText == null){
-	chrome.storage.set({'text':[concept.text]});
+    if (chrome.storage.local.get('text',function(){}) == null){
+	chrome.storage.local.set({'text' : Array(concept.text)});
     }
     else{
-	chrome.storage.set({'text':currentText.push(concept.text)});
+	x = chrome.storage.local.get('text',function(){});
+	x.push(concept.text);
+	chrome.storage.local.set({'text':x});
     }
-    if (currentRelevance == null){
-	chrome.storage.set({'relevance':[concept.relevance]});
+    if (chrome.storage.local.get('relevance',function(){}) == null){
+	chrome.storage.local.set({'relevance':Array(concept.relevance)});
     }
     else{
-	chrome.storage.set({'relevance':currentText.push(concept.relevance)});
+	x = chrome.storage.local.get('relevance',function(){});
+	x.push(concept.relevance);
+	chrome.storage.local.set({'relevance':x});
     }
 }
