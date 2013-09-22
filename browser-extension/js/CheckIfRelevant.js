f11b50d6d04dbcc952908422ff6f8f8ecc9efd85
kingofirony <kingofirony@gmail.com>
2013-09-22 12:19:19 -0400

    works for real this time

--- browser-extension/js/CheckIfRelevant.js
+++ browser-extension/js/CheckIfRelevant.js
@@ -1,35 +1,33 @@
-var currentUrl = window.location ;
-var corpus,currentPageCorpus;
-var verified = false;
+var currentUrl = window.location;
+var corpus, currentPageCorpus;
+var verified = 0;
+
 getJsonResponseForURL(currentUrl);
 
 setTimeout(function(){
- chrome.runtime.sendMessage({request: "sendText"}, function(response) {
-	 currentPageCorpus=arrayOfConcepts;
-	corpus = response.text;
-	alert(corpus);
-	alert(currentPageCorpus[0].text);
-	verify();
-     });},1000);
+	chrome.runtime.sendMessage({request: "sendText"}, function(response) {
+		currentPageCorpus=arrayOfConcepts;
+		corpus = response.text;
+		    verify();
+
+	    });},1000);
 
 
 function verify(){
-
-for(var i = 0;i<corpus.length;i++){
-    for(var y=0;y<currentPageCorpus;y++){
-	
-	if(corpus[i] === currentPageCorpus[y].text)
-	    {
-		verified=true;
+    for(var i = 0;i<corpus.length;i++){
+	for(var y=0;y<currentPageCorpus.length ;y++){
+	    
+	    if((corpus[i] + "") == (currentPageCorpus[y].text + ""))
+		{
+		    verified=true;
 	    }
+	}
     }
-}
 
-if(!verified){
-window.location  = chrome.extension.getURL('/html/badboy.html');
-}
-
-}
+    if(verified ==false){
+   window.location = chrome.extension.getURL('/html/badboy.html');
+    }
+   }
 
    
 
